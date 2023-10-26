1b7a22770cf2bba6426247b7f6334e08ead76f2a
blaisedegrille <47615137+blaisedegrille@users.noreply.github.com>
2023-10-26 21:39:01 +0200

    feat: add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyALyyypNrGwGCH3R_c3GUBzIWnEwR1mXXE',
+  authDomain: 'bdg-editor.firebaseapp.com',
+  projectId: 'bdg-editor',
+  storageBucket: 'bdg-editor.appspot.com',
+  messagingSenderId: '456818769012',
+  appId: '1:456818769012:web:7247b16a63f7677f506521',
+  measurementId: 'G-RVBHV19XKQ',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = analytics(app);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+function initializeApp(firebaseConfig: {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+}) {
+  throw new Error('Function not implemented.');
+}
