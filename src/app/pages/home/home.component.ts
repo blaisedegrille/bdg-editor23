c5b10535487c563023a54b61c710ac04e420a06d
blaisedegrille <47615137+blaisedegrille@users.noreply.github.com>
2023-11-05 21:59:42 +0100

    feat: update home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import * as EditorJS from '@editorjs/editorjs';
+import { config } from 'rxjs';
+import { EditorService } from 'src/app/service/editor.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  editorService = inject(EditorService);
 
+  ngOnInit(): void {
+    console.log('home oninit: ', this.editorService.data);
+    // this.editor = new EditorJS(config);
+  }
 }
